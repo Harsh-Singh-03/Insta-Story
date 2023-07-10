98387947304fa49bb49b270beb7c4013d3153fa4
Harsh Singh <harshnov802@gmail.com>
2023-07-10 21:56:29 +0530

    Fixing next prev slides some bugs

--- story.js
+++ story.js
@@ -67,94 +67,10 @@ imgDOM.forEach((ele, i) =>{
     })
 })
 
-// let Items = document.querySelectorAll('.slide-items')
-// let Thumb = document.querySelectorAll('.slide-thumb')
-
-// class SlideStories {
-//     constructor(id, index) {
-//         this.slide = document.querySelector(`[data-slide=${id}]`);
-//         console.log(this.slide)
-//         this.active = 0;
-//         this.init(index);
-//     }
-
-//     activeSlide(index, i) {
-//       console.log(index)
-//         this.active = index;
-//         this.items.forEach((item) => {
-//             item.classList.remove("active");
-//         });
-//         this.items[index].classList.add("active");
-//         this.thumbItems.forEach((item) => {
-//             item.classList.remove("active");
-//         });
-//         this.thumbItems[index].classList.add("active");
-//         this.autoSlide(i);
-//     }
-
-//     prev(i) {
-//         console.log(i, this.active)
-//         if (this.active > 0) {
-//             this.activeSlide(this.active - 1);
-//         } else {
-//           // this.activeSlide(this.items.length - 1);// change(i)
-//         }
-//     }
-
-//     next(i) {
-//       console.log(i, this.active, this.items.length - 1)
-//         if (this.active < this.items.length - 1) {
-//             this.activeSlide(this.active + 1);
-//         } else {
-//           // this.activeSlide(0);
-//         }
-//     }
-//     addNavigation(i) {
-//         const nextBtn = this.slide.querySelector(`.slide-next${i}`);
-//         const prevBtn = this.slide.querySelector(`.slide-prev${i}`);
-//         console.log(nextBtn, prevBtn)
-//         nextBtn.addEventListener("click", this.next(i));
-//         prevBtn.addEventListener("click", this.prev(i));
-//     }
-
-//     addThumbItems() {
-//         this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
-//         this.thumbItems = Array.from(this.thumb.children);
-//     }
-
-//     autoSlide(i) {
-//         clearTimeout(this.timeout);
-//         this.timeout = setTimeout(this.next(i), 15000);
-//     }
-
-//     init(i) {
-//         this.next = this.next.bind(this);
-//         this.prev = this.prev.bind(this);
-//         this.items = this.slide.querySelectorAll(".slide-items > *");
-//         this.thumb = this.slide.querySelector(".slide-thumb");
-//         console.log(this.thumb)
-//         this.addThumbItems();
-//         this.activeSlide(0, i);
-//         this.addNavigation(i);
-//     }
-// }
-
-
-// const doneClose = () => {
-//     Items.innerHTML = '';
-//     Thumb.innerHTML = "";
-//     mainWrap.style.display = "none";
-//     storyContainer.style.display = "flex"
-// }
-// const close = document.querySelectorAll(".closeStory")
-// close.forEach(ele =>{
-//   ele.addEventListener("click", doneClose)
-// })
-
 var swiper = new Swiper(".mySwiper", {
   effect: "cube",
   allowTouchMove: false,
-  loop: true,
+//   loop: true,
   speed: 1000,
   cubeEffect: {
     shadow: true,
@@ -184,32 +100,28 @@ const NextEl = (i) =>{
         span[acitveIndex].classList.add("active")
         clearInterval(interval); 
         interval = setInterval(() => {
-            console.log("hello")
             NextEl(swiper.activeIndex)
         }, 10000);
     }else{
-        let imgNex = ""
-        let spanNext = ""
-        if(i === containerArr.length - 1){
-            imgNex = imgDOM[0].querySelectorAll('img')
-            spanNext = thumb[0].querySelectorAll('span')
+        if(i !== containerArr.length -1){
+            img.forEach((Element,index)=>{
+                Element.classList.remove("active-img")
+                span[index].classList.remove("active")
+            })
+         let  imgNex = imgDOM[i+1].querySelectorAll('img')
+         let  spanNext = thumb[i+1].querySelectorAll('span')
+         acitveIndex = 0
+         imgNex[0].classList.add("active-img")
+         spanNext[0].classList.add("active")
+         swiper.slideNext()
+         clearInterval(interval); 
+         interval = setInterval(() => {
+             NextEl(swiper.activeIndex)
+         }, 11000);
         }else{
-            imgNex = imgDOM[i+1].querySelectorAll('img')
-           spanNext = thumb[i+1].querySelectorAll('span')
+            acitveIndex = 0
         }
-        acitveIndex = 0
-        imgNex.forEach((el,ind)=>{
-            el.classList.remove('active-img')
-            spanNext[ind].classList.remove("active")
-        })
-        imgNex[0].classList.add("active-img")
-        spanNext[0].classList.add("active")
-        swiper.slideNext()
-        clearInterval(interval); 
-        interval = setInterval(() => {
-            console.log("hello")
-            NextEl(swiper.activeIndex)
-        }, 11000);
+       
     }
 }
 
@@ -226,32 +138,27 @@ const PrevEl = (i) =>{
         span[acitveIndex].classList.add("active")
         clearInterval(interval); 
         interval = setInterval(() => {
-            console.log("hello")
             NextEl(swiper.activeIndex)
         }, 10000);
     }else{
-        let imgNex = ""
-        let spanNext = ""
-        if(i === 0){
-            imgNex = imgDOM[containerArr.length - 1].querySelectorAll('img')
-            spanNext = thumb[containerArr.length - 1].querySelectorAll('span')
+        if(i !== 0){
+            img.forEach((Element,index)=>{
+                Element.classList.remove("active-img")
+                span[index].classList.remove("active")
+            })
+         let  imgNex = imgDOM[i-1].querySelectorAll('img')
+         let  spanNext = thumb[i-1].querySelectorAll('span')
+         acitveIndex = 0
+         imgNex[0].classList.add("active-img")
+         spanNext[0].classList.add("active")
+         swiper.slidePrev()
+         clearInterval(interval); 
+         interval = setInterval(() => {
+             NextEl(swiper.activeIndex)
+         }, 11000);
         }else{
-            imgNex = imgDOM[i-1].querySelectorAll('img')
-           spanNext = thumb[i-1].querySelectorAll('span')
+            acitveIndex = 0
         }
-        acitveIndex = 0
-        imgNex.forEach((el,ind)=>{
-            el.classList.remove('active-img')
-            spanNext[ind].classList.remove("active")
-        })
-        imgNex[0].classList.add("active-img")
-        spanNext[0].classList.add("active")
-        swiper.slidePrev()
-        clearInterval(interval); 
-        interval = setInterval(() => {
-            console.log("hello")
-            NextEl(swiper.activeIndex)
-        }, 11000);
     }
 }
 
@@ -261,7 +168,7 @@ close.forEach(ele =>{
     ele.addEventListener('click', function(){
         storyDOM.style.display= "none"
         storyContainer.style.display = "flex"
-        
+        clearInterval(interval); 
     })
 })
 const openStory = (i) => {
@@ -274,7 +181,6 @@ const openStory = (i) => {
     swiper.slideTo(i)
     swiper.update();
     interval = setInterval(() => {
-        console.log("hello")
         NextEl(swiper.activeIndex)
     }, 10000);
  }
